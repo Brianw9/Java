0c1bc37ed8334fc4e81e163d49fe09b588a705a2
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2017-12-22 20:41:05 -0500

    Add files via upload

--- pi_length.java
+++ pi_length.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
-public class Pi{
+public class pi_length{
 	public static void main(String args[]){
 		double pi = Math.PI;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("How many digits of Pi?");
 		int input = scan.nextInt();
-		System.out.println("You want Pi to the digit: " + input);
-		String answer = Double.toString((double) pi);
-		String cutstring = answer.substring(0, input+2);
-		System.out.println("Here is your pi value: "+cutstring);
 		scan.close();
+			if(input>=0&&input<=15){
+				System.out.println("You want Pi to the digit: " + input);
+				String answer = Double.toString((double) pi);
+				String cutstring = answer.substring(0, input+2);
+				System.out.println("Here is your pi value: "+cutstring);
+				}
+		else{
+			System.out.println("We dont have the technology for that yet");
+		}
 	}
 }
