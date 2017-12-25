de3443d47881c3c342282b5347393f93a867097c
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2017-12-25 16:53:06 -0500

    Add files via upload

--- Collatz.java
+++ Collatz.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Collatz 
 {
+	int i = 0;
 	public static void main(String[] args)
 	{
 		System.out.println("Number to parse: ");  
@@ -11,16 +12,23 @@ public class Collatz
 		scan.close();
 		while (i != 1)
 		{
-			if ( i % 2 == 0)
-			{
-				i = i / 2;
-				System.out.println(i);
-			}
-			else if ( i % 2 == 1)
-			{
-				i = i * 3 + 1;
-				System.out.println(i);
-			}
+			i = Collatz.run(i);
+			System.out.println(i);
+		}
+	}
+	public static int run(int i){
+		if ( i % 2 == 0)
+		{
+			i = i / 2;
+			return i;
+		}
+		else if ( i % 2 == 1)
+		{
+			i = i * 3 + 1;
+			return i;
+		}
+		else{
+			return i;
 		}
 	}
 }
